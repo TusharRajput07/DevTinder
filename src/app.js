531283de6f5f3b7acded747618658c3254bccad7
TusharRajput07 <tusharrajput2002@gmail.com>
2025-02-11 19:42:19 +0530

    created basic apis

--- src/app.js
+++ src/app.js
@@ -19,6 +19,62 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// api to get a user with email from db
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+
+  try {
+    const users = await User.find({ email: userEmail }); // returns an array of all the matching docs with this email
+    if (users.length === 0) {
+      res.send("user not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    req.status(400).send("Something went wrong");
+  }
+});
+
+// api for the feed page : getting all the users from the db
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({}); // empty filter returns all data
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// api to delete a user from db (by id)
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// api to update a user's data
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    await User.findByIdAndUpdate(userId, data);
+    // or
+    // const user = await User.findByIdAndUpdate(userId, data, {
+    //   returnDocument: "before",
+    // });
+    // console.log(user);
+    res.send("User data updated successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 // calling the connectDB finction first. only then we'll start listening on port 3000
 connectDB()
   .then(() => {
