334ff46783a09fd0d7ff380efbe8ceaccd87d435
TusharRajput07 <tusharrajput2002@gmail.com>
2025-02-17 23:03:10 +0530

    added db validation, login, profile apis, jwt token and cookies

--- src/app.js
+++ src/app.js
@@ -1,21 +1,99 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./config/model/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 // middleware provided by express to convert req json object to javascript object for all the below API's
 app.use(express.json());
 
+app.use(cookieParser());
+
 // api to save a user to the database
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
-
   try {
+    const { firstName, lastName, email, password } = req.body;
+
+    // validate the data
+    validateSignUpData(req);
+
+    // encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // store the data
+
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
+
     await user.save();
     res.send("User added successfully");
   } catch (err) {
-    res.status(400).send("Error saving the user" + err.message);
+    res.status(400).send("Error saving the user : " + err.message);
+  }
+});
+
+// api to login a user
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check if user with this email is present in the db or not
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+    // if yes, then compare the password with the hashed db password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      // create a jwt token
+      const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$123");
+
+      // add the token to cookie
+      res.cookie("token", token);
+
+      // send the response
+      res.send("login successfull!");
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong : " + err.message);
+  }
+});
+
+// api to get the profile of user by cookie token after authentication
+app.get("/profile", async (req, res) => {
+  try {
+    // fetch the token from the cookie
+    const cookies = req.cookies;
+    const { token } = cookies;
+    if (!token) {
+      throw new Error("Invalid Token");
+    }
+
+    // authentication : verify the token with passkey
+    const jwtData = await jwt.verify(token, "DEV@Tinder$123");
+    const { _id } = jwtData;
+
+    // find the user by id
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found. Login Again");
+    }
+
+    // send the response data
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong : " + err.message);
   }
 });
 
@@ -58,11 +136,21 @@ app.delete("/user", async (req, res) => {
 });
 
 // api to update a user's data
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
 
   try {
+    const ALLOWED_UPDATES = ["bio", "age", "gender", "photoURL"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("update not allowed");
+    }
+
     await User.findByIdAndUpdate(userId, data);
     // or
     // const user = await User.findByIdAndUpdate(userId, data, {
@@ -71,7 +159,7 @@ app.patch("/user", async (req, res) => {
     // console.log(user);
     res.send("User data updated successfully");
   } catch (err) {
-    res.status(400).send("Something went wrong");
+    res.status(400).send("Update Failed : " + err.message);
   }
 });
 
