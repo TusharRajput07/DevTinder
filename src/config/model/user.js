5238211acce981514b3390a1b83d9924044eef71
TusharRajput07 <tusharrajput2002@gmail.com>
2025-02-17 23:03:10 +0530

    added db validation, login, profile apis, jwt token and cookies

--- src/config/model/user.js
+++ src/config/model/user.js
@@ -1,15 +1,57 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 // a schema for a user of DevTinder. and creating a model of it and exporting exporting it
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  email: { type: String },
-  age: { type: Number },
-  gender: { type: String },
-  bio: { type: String, maxlength: 300 },
-});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, trim: true },
+    email: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email address not valid : " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password : " + value);
+        }
+      },
+    },
+    age: { type: Number, min: 18, max: 100 },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("Gender can only be male, female or other");
+        }
+      },
+    },
+    bio: { type: String, maxlength: 300 },
+    photoURL: {
+      type: String,
+      default:
+        "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("photo url not valid : " + value);
+        }
+      },
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
